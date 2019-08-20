4a945440c91f277051ecf5fca30057ca2947bf35
svetlanateleshik <33606833+svetlanateleshik@users.noreply.github.com>
2019-08-20 23:03:29 +0300

    add

--- alexreznikov/js/scripts.js
+++ alexreznikov/js/scripts.js
@@ -1,26 +1,35 @@
 $(document).ready(function () {
 
-$('.main-slider').slick({
-  dots: true,
-  infinite: true,
-  speed: 250,
-  fade: true,
-  cssEase: 'linear'
+
+
+ var time = 2;
+ $(window).scroll(function(){
+    $('#counter').each(function(){
+      var
+      cPos = $(this).offset().top,
+      topWindow = $(window).scrollTop(); 
+    if (cPos < topWindow + 200) {
+    $('div').each(function(){
+      var 
+      i = 1,
+      num = $(this).data('num'),
+      step = 1000 * time / num,
+      that = $(this),
+      int = setInterval(function(){
+        if (i <= num) {
+          that.html(i);
+        }
+        else {
+          clearInterval(int);
+        }
+        i++;
+      },step);
+    });
+  }
+});  
 });
 
-
-$('.about-slider').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
 });
-
-   
-});
-
-
 
 
 
