7082f2d409e0e47f4dbbb2c41e10fd74272ec4cb
svetlanateleshik <33606833+svetlanateleshik@users.noreply.github.com>
2019-08-25 21:20:36 +0300

    add

--- alexreznikov/js/scripts.js
+++ alexreznikov/js/scripts.js
@@ -2,30 +2,33 @@ $(document).ready(function () {
 
 
 
- var time = 2;
+ var time = 2, cc = 1;
  $(window).scroll(function(){
     $('#counter').each(function(){
       var
       cPos = $(this).offset().top,
       topWindow = $(window).scrollTop(); 
-    if (cPos < topWindow + 200) {
-    $('div').each(function(){
-      var 
-      i = 1,
-      num = $(this).data('num'),
-      step = 1000 * time / num,
-      that = $(this),
-      int = setInterval(function(){
-        if (i <= num) {
-          that.html(i);
-        }
-        else {
-          clearInterval(int);
-        }
+      if (cPos < topWindow + 200) {
+        if (cc < 2) {
+          $('div').each(function(){
+          var 
+          i = 1,
+          num = $(this).data('num'),
+          step = 1000 * time / num,
+          that = $(this),
+          int = setInterval(function(){
+            if (i <= num) {
+              that.html(i);
+          }
+          else {
+            cc = cc + 2;
+            clearInterval(int);
+          }
         i++;
       },step);
     });
   }
+}
 });  
 });
 
